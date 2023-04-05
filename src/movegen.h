a48b9c79e1301d6de903d7f2a7f1270ce9dbad33
Calcitem <calcitem@outlook.com>
2023-04-05 18:35:33 +0800

    5

--- src/movegen.h
+++ src/movegen.h
@@ -46,14 +46,14 @@ inline bool operator<(const ExtMove& f, const ExtMove& s) {
 }
 
 
-ExtMove* generate(const Position& pos, ExtMove* moveList);
+ExtMove* generate(Position& pos, ExtMove* moveList);
 
 /// The MoveList struct is a simple wrapper around generate(). It sometimes comes
 /// in handy to use this class instead of the low level generate() function.
 template<GenType T>
 struct MoveList {
 
-  explicit MoveList(const Position& pos) : last(generate(pos, moveList)) {}
+  explicit MoveList(Position& pos) : last(generate(pos, moveList)) {}
   const ExtMove* begin() const { return moveList; }
   const ExtMove* end() const { return last; }
   size_t size() const { return last - moveList; }
